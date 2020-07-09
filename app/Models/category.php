e3ad6edccb240f7e423394652124eba79d2422c9
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-07-09 17:42:12 +0900

    categoryモデル作成

---
+++ app/Models/category.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class category extends Model
+{
+    //テーブル名 
+    protected $table = 'categories';
+
+    //可変項目
+    protected $fillable =
+    [
+        'cat_name',
+    ];
+}
