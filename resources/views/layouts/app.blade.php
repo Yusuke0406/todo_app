1dfcfeb69d619b412a36938a92e3b16d4cd22a8d
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-07-14 16:49:55 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script type="text/javascript" src="./js/recognition.js"></script>
+    <script type="text/javascript" src="./js/recognition2.js"></script>
+    <script type="text/javascript" src="./js/recognition3.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
